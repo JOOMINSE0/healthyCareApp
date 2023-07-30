87fe4f07d0fb035aaef9331b30a9c533fee782ff
vinxenzo <wonhostred93@gmail.com>
2023-07-30 12:27:24 +0900

    googlemap api

---
+++ app/src/main/java/com/example/healthycareapp/MapsFragment.kt
@@ -0,0 +1,107 @@
+package com.example.healthycareapp
+
+import android.content.Intent.getIntent
+import android.graphics.drawable.BitmapDrawable
+import android.location.Location
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.FragmentActivity
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapsFragment(search: Search) : Fragment(), OnMapReadyCallback {
+
+    lateinit var locationPermission: ActivityResultLauncher<Array<String>>
+
+    private lateinit var mMap: GoogleMap
+
+    lateinit var fusedLocationClient: FusedLocationProviderClient
+    lateinit var locationCallback: LocationCallback
+
+    companion object {
+        fun newInstance(): MapsFragment {
+            return MapsFragment()
+        }
+
+        private fun MapsFragment(): MapsFragment {
+            val mapsFragment = MapsFragment.newInstance()
+            return mapsFragment
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        locationPermission = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { results ->
+            if (!results.all { it.value }) {
+                Toast.makeText(activity, "권한 승인이 필요합니다.", Toast.LENGTH_LONG).show()
+            }
+        }
+        return inflater.inflate(R.layout.fragment_maps, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+
+        mapFragment?.getMapAsync(this)
+    }
+
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+
+        //camera 및 marker 설정
+        mMap.apply {
+            mMap.clear()
+            val seoul = LatLng(37.566418, 126.977943)
+
+            val markerOptions = MarkerOptions()
+                .position(seoul)
+                .title("Seoul")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
+
+            mMap?.addMarker(markerOptions)
+
+            val cameraPosition = CameraPosition.Builder()
+                .target(seoul)
+                .zoom(15.0f)
+                .build()
+            val cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition)
+            mMap.moveCamera(cameraUpdate)
+
+        }
+
+    }
+    private fun Marker?.icon(defaultMarker: BitmapDescriptor) {
+
+    }
+}
--- app/src/main/java/com/example/healthycareapp/Search.kt
+++ app/src/main/java/com/example/healthycareapp/Search.kt
@@ -4,19 +4,30 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.gms.maps.GoogleMap
 
 class Search : AppCompatActivity() {
+    private lateinit var mapView: GoogleMap
+
     lateinit var homeButton: Button
     lateinit var beforeButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        //지도 연결
+        val fm = supportFragmentManager
+        val fragmentTransaction = fm.beginTransaction()
+        val mapsFragment = MapsFragment(this)
+        fragmentTransaction.add(R.id.map, mapsFragment)
+        fragmentTransaction.commit()
 
         //선언(home)
         homeButton = findViewById<Button>(R.id.homeButton)
 
         //동작
-        homeButton.setOnClickListener{
+        homeButton.setOnClickListener {
             var intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
@@ -26,12 +37,10 @@ class Search : AppCompatActivity() {
         beforeButton = findViewById<Button>(R.id.beforeButton)
 
         //동작
-        beforeButton.setOnClickListener{
+        beforeButton.setOnClickListener {
             var intent = Intent(this, Result::class.java)
             startActivity(intent)
         }
-
-
 
 
     }
